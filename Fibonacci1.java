9d8c4dc3bce805b13c2479592df6e99781eca11d
ImalshaG <47671237+ImalshaG@users.noreply.github.com>
2019-02-22 11:11:50 +0530

    Naive Fibonacci algorithm-2

--- Fibonacci1.java
+++ Fibonacci1.java
@@ -4,11 +4,11 @@
 public class Fibonacci1 {
     public static void main(String args[]){
         Fibonacci1 fib1=new Fibonacci1();
-        System.out.println(fib1.getFib(10));
+        System.out.println(fib1.getFib(11));
 
     }
     public int getFib(int num){
-        if (num==1){
+        if (num==2 || num==1){
             return 1;
         }else if (num==0){
             return 0;

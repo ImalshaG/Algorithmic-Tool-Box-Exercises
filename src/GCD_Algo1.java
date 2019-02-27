1c3cc22f570f452cc58b43933dfa12c238121439
ImalshaG <imalsha.sg@gmail.com>
2019-02-27 21:38:59 +0530

    Error checked

--- src/GCD_Algo1.java
+++ src/GCD_Algo1.java
@@ -4,8 +4,8 @@
 public class GCD_Algo1 {
     public int getGCDalgo1(int num1, int num2){
         int numMin=Math.min(num1,num2);
-        int maxGCD=1;
-        for (int div=numMin;div>=2;div--){
+        int maxGCD=0;
+        for (int div=numMin;div>=1;div--){
             if(num1%div==0 && num2%div==0){
                 maxGCD=div;
                 break;
--- src/MainClass.java
+++ src/MainClass.java
@@ -4,21 +4,24 @@ import java.util.Random;
  */
 public class MainClass {
     public static void main(String args[]){
+
         Random r=new Random();
-        int num1=r.nextInt(100);
-        int num2=r.nextInt(100);
-
-        GCD_Algo1 test1=new GCD_Algo1();
-        int out1=(test1.getGCDalgo1(num1,num2));
-        GCD_AlgoEfficient test2= new GCD_AlgoEfficient();
-        int out2=(test2.euclidGCD(num1,num2));
-
         while(true){
+            int num1=r.nextInt(10)+1;
+            int num2=r.nextInt(10)+1;
+
+            GCD_Algo1 test1=new GCD_Algo1();
+            int out1=(test1.getGCDalgo1(num1,num2));
+            GCD_AlgoEfficient test2= new GCD_AlgoEfficient();
+            int out2=(test2.euclidGCD((num1),num2));
+
             if (out1==out2){
                 System.out.println("OK");
             }else {
                 System.out.printf("num1 = %d num2 = %d", num1, num2);
+                System.out.print("");
                 System.out.printf("output1 = %d output2 = %d", out1, out2);
+                System.out.print("");
                 break;
             }
         }

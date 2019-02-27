2c927d2ab3b50fc80eba09c240da2c37c08c9ed5
ImalshaG <imalsha.sg@gmail.com>
2019-02-27 21:09:46 +0530

    Stress testing using the 2 algorithms

--- src/GCD_AlgoEfficient.java
+++ src/GCD_AlgoEfficient.java
@@ -3,10 +3,6 @@
  */
 public class GCD_AlgoEfficient {
 
-    public static void main(String args[]){
-        GCD_AlgoEfficient test2= new GCD_AlgoEfficient();
-        System.out.println(test2.euclidGCD(22134,357567));
-    }
     public int euclidGCD(int a,int b){
         int aRemainder;
         if (b==0){
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,26 @@
+import java.util.Random;
 /**
  * Created by DELL on 2/27/2019.
  */
 public class MainClass {
     public static void main(String args[]){
-        GCD_Algo1 test1=new GCD_Algo1();
-        System.out.println(test1.getGCDalgo1(55,22));
+        Random r=new Random();
+        int num1=r.nextInt(100);
+        int num2=r.nextInt(100);
 
+        GCD_Algo1 test1=new GCD_Algo1();
+        int out1=(test1.getGCDalgo1(num1,num2));
+        GCD_AlgoEfficient test2= new GCD_AlgoEfficient();
+        int out2=(test2.euclidGCD(num1,num2));
+
+        while(true){
+            if (out1==out2){
+                System.out.println("OK");
+            }else {
+                System.out.printf("num1 = %d num2 = %d", num1, num2);
+                System.out.printf("output1 = %d output2 = %d", out1, out2);
+                break;
+            }
+        }
     }
 }
